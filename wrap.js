73d194fa3e33058353017058c7c4ad0f2dcc360b
Amanda G <amagonzalez27@gmail.com>
2018-11-19 11:53:15 -0500

    hi

--- wrap.js
+++ wrap.js
@@ -1,3 +1,21 @@
-const wrap = (line, maxLen) => '';
+const wrap = (line, maxLen) => {
+  if (line.length === 0) {
+    return '';
+  } else {
+    let indexSpace = -1;
+    let indexPrevSpace = 0;
+    let newLine = '';
+    let temp = '';
+    while (indexSpace < line.length) {
+      indexSpace = line.indexOf(' ', indexSpace + 1); // 5
+      if (indexSpace <= maxLen) {
+        temp += line.slice(indexPrevSpace, indexSpace);
+        indexPrevSpace = indexSpace;
+      } else {
+        newLine += `${temp}\n`;
+      }
+    }
+  }
+};
 
 module.exports = wrap;
--- wrap.spec.js
+++ wrap.spec.js
@@ -2,7 +2,15 @@ const { expect } = require('chai');
 const wrap = require('./wrap');
 
 describe('wrap', () => {
-    it('Returns expty string if empty string was provided', () => {
-        expect(wrap("", 10)).to.equal("");
-    });
-})
+  let line =
+    'Lorem Ipsum is simply dummy text of the printing and typesetting industry.';
+
+  it('Returns empty string if empty string was provided', () => {
+    expect(wrap('', 10)).to.equal('');
+  });
+
+  it('should be able to print one line if maxLen is greater than line length', () => {
+    let shortLine = line.slice(0, 4);
+    expect(wrap(shortLine, 5)).to.equal('Lore');
+  });
+});
